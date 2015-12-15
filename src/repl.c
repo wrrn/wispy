e9e02b0b38254deb376ea79fbdcb4da10e1b7017
Warren Harper <warrenjharper@gmail.com>
2015-12-14 20:32:57 -0500

    Evaluate ast

--- src/repl.c
+++ src/repl.c
@@ -8,6 +8,35 @@
 #endif
 
 
+long eval(mpc_ast_t* t) {
+  /* If tagged a number returns directly */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  char *op = t->children[1]->contents;
+  long x = eval(t->children[2]);
+
+  int i = 3;
+
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, (eval(t->children[i])));
+    i += 1;
+  }
+
+  return x;
+
+  
+}
+
+long eval_op(long x, char *op, long y) {
+  if (strcmp(op, "+")) { return x + y; }
+  if (strcmp(op, "-")) { return x - y; }
+  if (strcmp(op, "*")) { return x * y; }
+  if (strcmp(op, "/")) { return x / y; }
+  if (strcmp(op, "%")) { return x % y; }
+}
+
 
 int main(int argc, char **argv) {
 
@@ -46,7 +75,8 @@ int main(int argc, char **argv) {
     mpc_result_t r;
     if(mpc_parse("<stdin>", input, Lispy, &r)) {
       /* On Success print the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise print error */
@@ -62,5 +92,3 @@ int main(int argc, char **argv) {
   return 0;
 
 }
-
-
